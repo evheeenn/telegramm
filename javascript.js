f6a85bcb101e377b545e42ef2ebaa3559990e44d
evheeenn <122995202+evheeenn@users.noreply.github.com>
2023-01-28 20:56:07 +0200

    firsr commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const question_1 = prompt("Привет! Как тебя зовут?")
+const question_2 = prompt("Какая у тебя фамилия?")
+let year = prompt("Год рождения")
+
+alert("User bio:" + " " + question_1 + " " + question_2 + "," + " " + ("2023" - year) + " " + "years old")
+
+console.log("User bio:" + " " + question_1 + " " + question_2 + "," + " " + ("2023" - year) + " " + "years old")
