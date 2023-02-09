831fcfaec1ba5d75dd8e101d2f9446e109eff593
evheeenn <122995202+evheeenn@users.noreply.github.com>
2023-02-09 19:00:31 +0200

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-const question_1 = prompt("Привет! Как тебя зовут?")
-const question_2 = prompt("Какая у тебя фамилия?")
-let year = prompt("Год рождения")
-const currentYear = 2023
-
-alert("User bio:" + " " + question_1 + " " + question_2 + "," + " " + (currentYear - year) + " " + "years old")
-
-console.log("User bio:" + " " + question_1 + " " + question_2 + "," + " " + (currentYear - year) + " " + "years old")
