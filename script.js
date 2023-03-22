5f966955c6b6d91d2f157e422e6a7a81eb70517c
evheeenn <122995202+evheeenn@users.noreply.github.com>
2023-03-23 01:12:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const square = document.querySelector('#square')
+const sayBems = document.querySelector('#square p')
+square.style.left = '193px'
+square.style.top = '193px'
+document.addEventListener('keydown', (event) => {
+
+	if(event.keyCode === 37){
+		square.style.left = parseInt(square.style.left) - 10 + 'px'
+
+		if(parseInt(square.style.left) <= 0){
+			sayBems.style.visibility = 'inherit'
+			setTimeout(() => {
+				sayBems.style.visibility = 'hidden'
+				square.style.left = parseInt(square.style.left) + 20 + 'px'
+			}, 400)
+		}
+	}
+
+	if(event.keyCode == 39){
+		square.style.left = (parseInt(square.style.left) + 10) + 'px'
+
+		if(parseInt(square.style.left) >= 399){
+			sayBems.style.visibility = 'inherit'
+			setTimeout(() => {
+				sayBems.style.visibility = 'hidden'
+				square.style.left = parseInt(square.style.left) - 20 + 'px'
+			}, 400)
+		}
+	}
+ 
+	if(event.keyCode == 32){
+		square.style.top = (parseInt(square.style.top) - 100) + 'px'
+		setTimeout (() => {
+			square.style.top = (parseInt(square.style.top) + 100) + 'px'
+		},
+		2000)
+	}
+
+	if(event.keyCode == 17){
+		square.style.transform = 'scaleX(1.25) scaleY(0.4)'
+		setTimeout (() => {
+			square.style.transform = 'scaleX(1) scaleY(1)'
+		}, 2000)
+	}
+})
